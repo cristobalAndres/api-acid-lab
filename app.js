465d116080894db76d18931151e36d3eb169b6ac
Cristobal Palma <cristobal.palma@kastor.cl>
2018-07-30 18:07:39 -0400

    port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 const redis = require('redis');
 
 const redisClient = redis.createClient('redis://h:p910adb5a9a02a7bb608752d50cb8d0d6d17ee6a289495ac9155430275a39ca99@ec2-34-231-81-175.compute-1.amazonaws.com:9389');
+const port = process.env.PORT || 8000;
 
 redisClient.on('error', (err) => {
   console.log('ERROR REDIS', err);
@@ -19,6 +20,6 @@ app.get('/', function (req, res) {
   res.json('hola mundo');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
