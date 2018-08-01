418918b2a80341b1013b2e98ec9a3da73d248f8c
Cristobal Palma <cristobal.palma@kastor.cl>
2018-08-01 02:14:38 -0400

    socket

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 const WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({server: server})
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
