eb219ef931321cbd7c84184b6cfe7119cf9bc4d5
Cristobal Palma <cristobal.palma@kastor.cl>
2018-08-01 02:07:24 -0400

    socket

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const server = http.createServer(app)
 const WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({server: server})
 
-console.log("http server listening on %d", port);
-
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -21,47 +19,22 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/', function (req, res) {
-  // let citiesData = [];
-  // // Obtención de la información delas ciudades guardadas
-  // mixins.cities()
-  // .then((data) => {
-  //   citiesData = JSON.parse(data);
-  //   // Recorrido de las ciudades y consultar al servicio sobre temperatura y hora de la ciudad solicitada
-  //   const promises = citiesData.map(city => mixins.getData(city.latitude, city.longitude));
-  //   return Promise.all(promises);
-  // })
-  // .then((data) => {
-  //   res.json(data)
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.sendStatus(400);
-  // })
+app.get('/', (req, res) => {
+
 });
 
-wss.on('connection', function (ws) {
-  // ws.on('message', function (message) {
-  //   console.log('received: %s', message)
-  // })
-
-  // setInterval(
-  //   () => ws.send(mixins.initData().then((data) => {
-  //     return data;
-  //   })),
-  //   1000
-  // )
+wss.on('connection', (ws) => {
   const id = setInterval(() => {
     mixins.initData().then((data) => {
       ws.send(JSON.stringify(data));
     })
     .catch((error) => {
-      console.log('ERRROR', error);
+      console.log('ERROR', error);
     });
   }, 10000);
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
+  ws.on("close", () => {
+    console.log("websocket close")
     clearInterval(id)
   })
 })
